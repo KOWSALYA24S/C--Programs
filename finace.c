2f121a08a7fb8133f904dbb6a79da8f3f0e754d6
KOWSALYA24S <bl.sc.u4aie24021@bl.students.amrita.edu>
2024-12-04 14:36:45 +0530

    Add files via upload

---
+++ finace.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+float h,r,w,pay;
+printf("The no.of hours worked?");
+scanf("%f",&h);
+printf("The rate per hour worked?");
+scanf("%f",&r);
+printf("The no.of weeks in a month?");
+scanf("%f",&w);
+printf("Pay is %.2f\n",pay); //.2f for round off to 2 decimal places and \n for outputin next pline instead of same line
+}
